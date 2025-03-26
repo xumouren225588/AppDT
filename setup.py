e63cfa9ab66ac173e3f6671ba7a7ed7e9634f349
xumouren225588 <xumouren225588@163.com>
2025-03-26 19:50:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
  
 setup(
     name='AppDT',
-    version='2.0.0',
+    version='2.0.1',
     description='(This Is A Chinese Package)此工具箱可以帮您快速构建Windows™应用程序，也可以快速生成安装包',
     long_description=datatext,
     long_description_content_type="text/markdown",
@@ -12,7 +12,7 @@ setup(
     author_email='xumouren225588@163.com',
     url="https://github.com/xumouren225588/AppDT",
     packages=['AppDT'],
-    license='MIT',
+    license='Mulan Permissive Software License Version 2 (Mulan PSL v2)',
     install_requires=[
         'pyinstaller',
         'PyQt5',
